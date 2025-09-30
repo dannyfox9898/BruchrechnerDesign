b94357e75032132edfecc38d50e95bbe4edd8ee8
dannyfox9898 <gocaandjelkovic2004@gmail.com>
2025-09-30 09:54:55 +0200

    Initial commit

---
+++ src/Bruchrechner.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+public class Bruchrechner {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton a7Button;
+    private JButton a9Button;
+    private JButton xButton;
+    private JButton a8Button;
+    private JButton a4Button;
+    private JButton a6Button;
+    private JButton button11;
+    private JButton a5Button;
+    private JButton a3Button;
+    private JButton button14;
+    private JButton a2Button;
+    private JButton a1Button;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JButton button5;
+    private JButton button6;
+    private JButton a0Button;
+    private JButton button9;
+}
